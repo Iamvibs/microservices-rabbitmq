48c007650596f8642a2d99711b5da06420595d14
iamvibs <vm11082001@gmail.com>
2024-10-11 14:50:12 +0530

    fixed few things

--- internal/services/inventory_service/inventory/consumers/events/inventory_updated.go
+++
@@ -1,9 +0,0 @@
-package events
-
-import uuid "github.com/satori/go.uuid"
-
-type InventoryUpdated struct {
-	ProductId   uuid.UUID
-	InventoryId int64
-	Count       int32
-}
--- internal/services/inventory_service/inventory/consumers/events/product_created.go
+++
@@ -1,9 +0,0 @@
-package events
-
-import uuid "github.com/satori/go.uuid"
-
-type ProductCreated struct {
-	ProductId   uuid.UUID
-	InventoryId int64
-	Count       int32
-}
